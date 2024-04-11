dfdf44361b4a72541217abe7c2dc3c81f9ca6125
Julius Range-Lüdemann <julius.range@gmail.com>
2024-04-11 20:57:03 +0200

    Improve windows support

--- c_make.c
+++ c_make.c
@@ -17,8 +17,9 @@ C_MAKE_BUILD_ENTRY()
     if (c_make_compiler_is_msvc(target_c_compiler))
     {
         c_make_command_append(&command, "-nologo");
-        c_make_command_append(&command, c_make_c_string_concat("-Fe\"", c_make_c_string_path_concat(c_make_get_build_path(), "c_make.exe"), "\""));
-        c_make_command_append(&command, c_make_c_string_path_concat(c_make_get_source_path(), "c_make.c"));
+        c_make_command_append(&command, c_make_c_string_concat("-Fe", c_make_c_string_path_concat(c_make_get_build_path(), "c_make.exe")));
+        c_make_command_append(&command, c_make_c_string_concat("-Fo", c_make_c_string_path_concat(c_make_get_build_path(), "c_make.obj")));
+        c_make_command_append(&command, c_make_c_string_path_concat(c_make_get_source_path(), "c_make_wrapper.c"));
     }
     else
     {
